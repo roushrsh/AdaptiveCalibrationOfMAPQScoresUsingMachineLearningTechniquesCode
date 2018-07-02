1cbb78443597b6349e1595fb83484900c1e504dd
Roush <roushrsh@hotmail.com>
2018-07-02 15:41:12 +0200

    Update XGLRMergeSuggestionB.py

--- XGLRMergeSuggestionB.py
+++ XGLRMergeSuggestionB.py
@@ -1,23 +1,19 @@
 XGBoostScore = open("finalScores60MaxXG.txt", "r")
 LogisticScore = open("finalScores60Max.txt", "r")
-mergedResult = open("MERGED60MaxXGBoostLogisticRAWR2.txt", "w")
+mergedResult = open("MERGED60MaxXGBoostLogisticRAWR.txt", "w")
 
 from itertools import izip
 
 for x, y in izip(XGBoostScore, LogisticScore):
         x = x.strip()
         y = y.strip()
-        if (int(y)>44 & int(x)>22):
-                mergedResult.write(y)
-                mergedResult.write("\n")
-        elif (int(x) < 20):
+        if (int(x) > 30):
                 mergedResult.write(x)
                 mergedResult.write("\n")
-        elif (int(x) > 21):
+        elif (int(x) < 20):
                 mergedResult.write(x)
                 mergedResult.write("\n")
         else:
                 mergedResult.write(y)
                 mergedResult.write("\n")
-
 
